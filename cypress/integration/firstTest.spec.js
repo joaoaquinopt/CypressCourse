58382b1e86321172ba21d3a5f4efd133c9576cf6
artembondar <artem.bondar@aa.com>
2019-12-12 16:38:42 -0600

    then and wrap

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -39,7 +39,7 @@ describe('Our first suite', () => {
 
     })
 
-    it.only('find web elements', () => {
+    it('find web elements', () => {
 
         //cy.get('')
         //cy.find('')
@@ -67,6 +67,45 @@ describe('Our first suite', () => {
         cy.find('button')
     })
 
+    it.only('then and wrap', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email address')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+        //selenium
+        // const fistForm = cy.contains('nb-card', 'Using the Grid')
+        // const secondForm = cy.contains('nb-card', 'Basic form')
+
+        // fistForm.find('[for="inputEmail1"]').should('contain', 'Email')
+        // fistForm.find('[for="inputPassword2"]').should('contain', 'Password')
+        // secondForm.find('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        //cypress style
+
+        cy.contains('nb-card', 'Using the Grid').then( firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+            cy.contains('nb-card', 'Basic form').then( secondForm => {
+                const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(passwordLabelFirst).to.equal(passwordSecondText)
+
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+            })
+
+        })
+
+
+    })
 
 })
 
